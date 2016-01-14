700c3a9bc3d43cf34167c4b488ab4df979c49724
Isaac Johnson <hello@isaacjohnsononline.com>
2016-01-14 16:02:01 +1000

    added basic styles

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 // use express to initialize a function handler to be passed to a new node HTTP server
 var express = require('express');
-var http = require('http');
 var app = express();
-var server = http.Server(app);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
+app.use(express.static(__dirname + '/public'));
 // listen for get requests on the root directory and respond
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+});
+
 // listen on port 3000 for user connections
-server.listen(3000, function () {
+http.listen(3000, function () {
     console.log('listening on *:3000');
 });
