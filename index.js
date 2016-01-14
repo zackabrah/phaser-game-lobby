fdc72e74bd6b539f574ab54a85d706b05f158cf9
Isaac Johnson <hello@isaacjohnsononline.com>
2016-01-14 22:40:22 +1000

    basic client - server connection

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var rooms = [];
+var hosts = {};
+var clients = {};
 
+// create a unique random id for games
+function createRoomId() {
+    return (0|Math.random()*9e6).toString(36).substring(0,4);
+}
+
+// a helper function -> check if an element is within an array
+function in_array(search, array) {
+    return array.indexOf(search) >= 0;
+}
+
+// express static file middleware
 app.use(express.static(__dirname + '/src'));
 
 // listen for get requests on the root directory and respond
@@ -13,10 +27,56 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-    console.log('a user connected');
+    // add new socket id to the clients array
+    clients[socket.id] = socket;
+
+    // update server list for client
+    socket.emit('update-game-list', rooms);
 
     socket.on('disconnect', function () {
         console.log('user disconnected');
+
+        // remove the socket id from the clients array
+        clients[socket.id] = 0;
+    });
+
+    socket.on('join-game', function(data, ack) {
+
+    });
+
+    socket.on('host-game', function(data, ack) {
+
+        // this socket id is already a host
+        if (hosts[socket.id]) {
+
+            // TODO - handle error case
+            return false;
+        }
+
+        var roomID = createRoomId();
+
+        socket.join(roomID, function(err) {
+            if (!err) {
+
+                // this socket id is now a game host
+                hosts[socket.id] = true;
+
+                // create a room data meta object
+                var roomData = {"roomID":roomID, "playerCount":1, "hostID": socket.id};
+
+                // acknowledge the room has been successfully created
+                ack(roomData);
+
+                // emit the new room creation to all users
+                io.emit('update-game-list', [roomData]);
+
+                // store the room data on the server
+                rooms.push(roomData);
+
+            } else {
+                log(err, 'e');
+            }
+        });
     });
 });
 
